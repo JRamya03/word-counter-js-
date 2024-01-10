8284c299bc85939eb67f6a5842c05145972bec44
Ramya <ramyaj134@gmail.com>
2024-01-10 23:27:13 +0530

    counter

---
+++ script.js
@@ -0,0 +1,6 @@
+function convert(){
+    let count1 = document.getElementById("textarea").value
+    let count2= count1.length
+    let result=document.getElementById("result")
+    result.innerHTML = count2 + " words"
+   }
